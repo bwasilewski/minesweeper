16431fac54e2de4148cb1ee96a9449dde3ed81f2
Benjamin Wasilewski <ben.wasilewski@apervita.com>
2020-07-25 14:46:35 -0500

    draw a canvas with a grid of squares spread across it

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,10 +1,30 @@
-import '../styles/index.scss';
+import '../styles/index.scss'
 import * as p5 from 'p5'
 
 const P5 = new p5( (sk) => { 
 	sk.setup = () => {
-		sk.createCanvas(window.innerWidth, window.innerHeight)
+		sk.createCanvas(300, 300)
+		let r = sk.random(255)
+		let g = sk.random(255)
+		let b = sk.random(255)
+		sk.background(r, g, b)
 	}
-	sk.draw = () => {  }
+	sk.draw = () => {  
+		let xPos = 0
+		let yPos = 0
+		let size = 20
+
+		for ( let i = 0; i < 225; i++ ) {
+			sk.stroke('#cecece')
+			sk.strokeWeight(1)
+			sk.square(xPos, yPos, size)
+			if ( xPos === 280 ) {
+				xPos = 0
+				yPos += size
+			} else {
+				xPos += size
+			}
+		}
+	}
 })
 
